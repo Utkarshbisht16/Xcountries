80201550c7c4c438af1046b76a03bd83458f6758
utkarshbisht16 <utkarshbisht116@gmail.com>
2024-01-02 13:35:22 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import React,{useState, useEffect} from 'react';
+import React,{useState, useEffect,useRef} from 'react';
 
-
+const format = (timer) =>{
+  const mins = Math.floor(timer/60);
+  timer %= 60;
+  return `${mins} : ${timer < 10 ? "0" : ""}${timer}`
+}
 function App() {
-  const [data, setData] = useState([]);
-  const getData = async() =>{
-    try{
-      const res = await fetch('https://restcountries.com/v3.1/all');
-      const c = await res.json()
-      setData(c);
-    }catch(err){
-      console.log(err);
-    }
-  } 
+  const [click, setClick] = useState(false);
+  const [timer, setTimer] = useState(0);
+  const timerId = useRef(null);
+
+  const toggleHandler = () =>{
+    setClick(!click);
+  }
+  const reset = () =>{
+    setClick(false);
+    setTimer(0);
+  }
   useEffect(()=>{
-    getData();
-  },[])
+    timerId.current = setInterval(()=>{
+      if(click){
+        setTimer((prevTimer)=>prevTimer+1);
+      }
+    },1000);
+    return () =>{
+      clearInterval(timerId.current);
+    }
+  },[timer,click])
+
   return (
     <div className="App">
-      <div className='containerStyle'>
-        {data.map((item)=>{
-          return(
-            <div className='cardStyle'>
-              <img className='imageStyle' src={item.flags.png} alt="country-flag"/>
-              <h2>{item.name.common}</h2>
-            </div>
-          );
-        })}
-      </div>
+      <h1>Stopwatch</h1>
+      <p>Time : {format(timer)}</p>
+      <button onClick={toggleHandler}>{click? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
