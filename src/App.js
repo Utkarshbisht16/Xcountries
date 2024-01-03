ce48648d693ef6154f7aef5cdc752d2233b191aa
utkarshbisht16 <utkarshbisht116@gmail.com>
2024-01-03 17:29:40 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -4,35 +4,16 @@ import React,{useState, useEffect,useRef} from 'react';
 
 
 function App() {
-  const [active, isActive] = useState(false);
-  const [toggle, isToggle] = useState(false);
-  const fName = useRef(null);
-  const sName = useRef(null);
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(c => c + 1);
+  const decrement = () => setCount(c => c - 1);
 
-  var show = (
-    <p>Full Name: {fName.current} {sName.current}</p>
-  )
-  const handel = (e) =>{
-    e.preventDefault();
-    var form = new FormData(document.getElementById("f"));
-    var fname = form.get("fname");
-    var sname = form.get("sname");
-    fName.current = fname;
-    sName.current = sname;
-    isActive(true);
-    isToggle(!toggle);
-  }
   return (
     <div className="App">
-      <h1>Full Name Display</h1>
-      <form id='f' onSubmit={handel}>
-        <label>First Name:</label>
-        <input required type='text' name='fname'></input><br/>
-        <label>Last Name:</label>
-        <input required type='text' name='sname'></input><br/>
-        <button type='submit'>Sumbit</button>
-      </form>
-      {active ? show : null}
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
