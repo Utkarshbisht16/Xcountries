ded74b77bfa5aec8a27ad0f49d47ced91a696651
utkarshbisht16 <utkarshbisht116@gmail.com>
2024-01-03 19:37:10 +0530

    cal

--- src/App.js
+++ src/App.js
@@ -4,18 +4,48 @@ import React,{useState, useEffect,useRef} from 'react';
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(c => c + 1);
-  const decrement = () => setCount(c => c - 1);
-
+  const [text, setText] = useState("");
+  const [ans, setAns] = useState("");
+  var handel = (val) =>{
+    const x = val.target.innerHTML;
+    if(x == '='){
+      if(text == ""){
+        setAns("Error");
+      }else{
+        let res = eval(text);
+        setAns(res);
+      }
+    }else if(x == 'C'){
+      setText("");
+      setAns("");
+    }else{
+      setText(t => t + x);
+    }
+  }
   return (
     <div className="App">
-      <h1>Counter App</h1>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <h1>React Calculator</h1>
+      <input type='text' value={text}></input>
+      <div id='result'>{ans}</div>
+      <button onClick={handel}>1</button>
+      <button onClick={handel}>2</button>
+      <button onClick={handel}>3</button>
+      <button onClick={handel}>+</button><br/>
+      <button onClick={handel}>4</button> 
+      <button onClick={handel}>5</button>
+      <button onClick={handel}>6</button>
+      <button onClick={handel}>-</button><br/>
+      <button onClick={handel}>7</button>
+      <button onClick={handel}>8</button>
+      <button onClick={handel}>9</button>
+      <button onClick={handel}>*</button><br/>
+      <button onClick={handel}>C</button>
+      <button onClick={handel}>0</button>
+      <button onClick={handel}>=</button>
+      <button onClick={handel}>/</button>
     </div>
   );
 }
 
 export default App;
+      
