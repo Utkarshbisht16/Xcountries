0d1dd862ff4402a68adb671361eaecf5c6c59f08
utkarshbisht16 <utkarshbisht116@gmail.com>
2024-01-07 16:14:07 +0530

    weather

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./styles.css";
-const customDictionary = {
-  teh: "the",
 
-  wrok: "work",
-
-  fot: "for",
-
-  exampl: "example",
-};
+import WeatherDisplay from "./component/WeatherDisplay";
+import Searchbar from "./component/Searchbar";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [suggestions, setSuggestions] = useState("");
-  const handleChange = (e) => {
-    const inputText = e.target.value;
-    setText(inputText);
-    const words = inputText.split(" ");
-    const correctedWords = words.map((el) => {
-      const correctedWord = customDictionary[el.toLowerCase()];
-      return correctedWord || el;
-    });
-    const correctedText = correctedWords.join(" ");
-    const firstCorrection = correctedWords.find((el, idx) => {
-      return el !== words[idx];
-    });
-    setSuggestions(firstCorrection || "");
-  };
+  const [isLoading, setIsLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("");
+  
+  const handleSearch = async (city) => {
+      
+    if(!city){
+      return
+    }
+    
+      try {
+        setIsLoading(true);
+        const response = await axios.get(
+          "https://api.weatherapi.com/v1/current.json",
+          {
+            params: {
+              key:"9e3e6c5fd5564cb7ade205952232610",
+              q:city
+              ,            
+            },
+          }
+        );
+             // console.log(response.data);
+        setWeatherData(response.data);
+        setCity(city);
+      } catch (err) {
+        
+        console.log("Error fetching data", err.message);
+        alert("Failed to fetch weather data");
+      } finally {
+        setIsLoading(false);
+        
+      }
+    
+    
+  }
+
+  useEffect(()=>{
+    handleSearch()
+  
+  },[city])
+
+  
+
+
   return (
-    <div>
-      <h1>Spell Check and Auto-Correction</h1>
-      <textarea
-        value={text}
-        onChange={handleChange}
-        placeholder="Enter text..."
-        rows={5}
-        cols={40}
-      />
-      {suggestions && (
-        <p>
-          Did you mean: <strong>{suggestions}</strong>?
-        </p>
-      )}
+    <div className="App">
+      <h1>Weather App</h1>
+     <Searchbar onSearch={()=>handleSearch(city)} city={city} setCity={setCity} placeholder="Enter city name"/> 
+      <WeatherDisplay weatherData={weatherData} isLoading={isLoading} />
     </div>
   );
 }
